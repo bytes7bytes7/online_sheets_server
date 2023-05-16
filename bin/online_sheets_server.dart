ebd42b752bd9e58f8391d0b10d2c9450ed768d71
bytes7bytes7 <bytes7bytes7@gmail.com>
2023-05-16 14:55:13 +0300

    add web socket

--- bin/online_sheets_server.dart
+++ bin/online_sheets_server.dart
@@ -1,5 +1,16 @@
-import 'package:online_sheets_server/online_sheets_server.dart' as online_sheets_server;
+import 'package:shelf/shelf_io.dart' as shelf_io;
+import 'package:shelf_web_socket/shelf_web_socket.dart';
+import 'package:web_socket_channel/web_socket_channel.dart';
 
-void main(List<String> arguments) {
-  print('Hello world: ${online_sheets_server.calculate()}!');
+void main() {
+  var handler = webSocketHandler((WebSocketChannel webSocket) {
+    webSocket.stream.listen((message) {
+      print(message);
+      webSocket.sink.add("echo $message");
+    });
+  });
+
+  shelf_io.serve(handler, '127.0.0.1', 8080).then((server) {
+    print('Serving at ws://${server.address.host}:${server.port}');
+  });
 }
--- test/online_sheets_server_test.dart
+++ test/online_sheets_server_test.dart
@@ -1,8 +1,1 @@
-import 'package:online_sheets_server/online_sheets_server.dart';
-import 'package:test/test.dart';
-
-void main() {
-  test('calculate', () {
-    expect(calculate(), 42);
-  });
-}
+void main() {}
