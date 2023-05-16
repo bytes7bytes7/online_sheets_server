58d352aa272f43b0ee60de529937441482fc1d30
bytes7bytes7 <bytes7bytes7@gmail.com>
2023-05-16 14:34:18 +0300

    init commit

---
+++ bin/online_sheets_server.dart
@@ -0,0 +1,5 @@
+import 'package:online_sheets_server/online_sheets_server.dart' as online_sheets_server;
+
+void main(List<String> arguments) {
+  print('Hello world: ${online_sheets_server.calculate()}!');
+}
---
+++ test/online_sheets_server_test.dart
@@ -0,0 +1,8 @@
+import 'package:online_sheets_server/online_sheets_server.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
